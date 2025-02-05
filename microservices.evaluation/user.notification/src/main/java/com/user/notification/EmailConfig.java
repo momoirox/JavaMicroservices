397a40d1fe537dc77f5767e1003b37d781f6b556
Maja Blagic <maja.blagic@naovis.com>
2025-02-05 21:20:21 +0100

    Goal: App run in local

---
+++ microservices.evaluation/user.notification/src/main/java/com/user/notification/EmailConfig.java
@@ -0,0 +1,40 @@
+package com.user.notification;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    @Value("${email.server.host}")
+    private String emailHost;
+
+    @Value("${email.server.port}")
+    private int emailPort;
+
+    @Value("${email.username}")
+    private String emailUsername;
+
+    @Value("${email.password}")
+    private String emailPassword;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(emailHost);
+        mailSender.setPort(emailPort);
+        mailSender.setUsername(emailUsername);
+        mailSender.setPassword(emailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
